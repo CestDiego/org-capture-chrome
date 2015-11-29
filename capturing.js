f9da504544381d474777c178f9c113ad4560b656
Michael Abrahams <miabraha@gmail.com>
2015-11-28 18:39:45 -0800

    Add link-only and link+text options
    These correspond to 'l' and 'c' org-capture templates.
    
    Also add comments and rename functions for readability.

--- capturing.js
+++ capturing.js
@@ -1,27 +1,51 @@
 var capture = function(config){
 
-    var replace_all = function(str, find, replace) {
-        return str.replace(new RegExp(find, 'g'), replace);
-    };
-    var esc = function (text) {
-        return replace_all(replace_all(replace_all(encodeURIComponent(text),
-                                                   "[(]",
-                                                   escape("(")),
-                                       "[)]",
-                                       escape(")")),
-                           "[']",
-                           escape("'"));
-    };
+    /**
+     * Escape all instances of a character @p char
+     */
+    String.prototype.esc = function(char) {
+        var rx = new RegExp("[" + char + "]", 'g');
+        return this.replace(rx, escape(char));
+    }
 
-    var uri = 'org-protocol://capture:/';
+    /**
+     * Escape all parentheses and single quotes in a string
+     */
+    var esc_parentheses_quote = function (text) {
+        return text.esc("(").esc(")").esc("'");
+    }
 
-    uri += config.template;
+    /**
+     * Construct a URI for the appropriate the org-capture template.
+     * TEMPLATE corresponds to org-capture-template hotkey.
+     *
+     * The general format is:
+     * org-protocol://capture://TEMPLATE/URL/TITLE/TEXT
+     */
+    var uri = 'org-protocol://capture://';
 
-    uri += '/' + encodeURIComponent(location.href)
-        +  '/' + encodeURIComponent(document.title);
+    // Only include text field if there is a selection in the webpage
+    var text = window.getSelection().toString();
+    if (text != "") { text = '/' + esc_parentheses_quote(text);};
 
-    var selection = window.getSelection().toString();
-    if (selection != "") { uri += '/' +	esc(selection); };
+    switch (config.template) {
+    case "c":
+        uri = uri + config.template + '/'
+            + encodeURIComponent(window.location.href) + '/'
+            + encodeURIComponent(document.title) 
+            + text;
+        break;
+    case "l":
+        uri = uri + config.template + '/'
+            + encodeURIComponent(location.href) + '/'
+            + encodeURIComponent(document.title);
+        break;
+    case "o":
+        uri = uri + encodeURIComponent(window.location.href) + '/'
+            + encodeURIComponent(document.title)
+            + text;
+        break;
+    }
 
     console.log("Preview of org-protocol URI: ", uri);
 
